f87b11227d1e6ec4a065207abb06442adb82da7d
crhistian.uv <crhistian.uv@gmail.com>
2024-04-01 16:30:19 -0600

    Finish exercise 2

---
+++ src/app/components/structural-directives/structural-directives.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-structural-directives',
+  templateUrl: './structural-directives.component.html',
+  styleUrl: './structural-directives.component.css'
+})
+export class StructuralDirectivesComponent {
+  public list: string[] = ["A", "B", "C"];
+}
